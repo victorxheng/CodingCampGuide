d9f95b21c3d17c6365b72ee9c0aa9cfba67ddf06
victorxheng <victorchengthesecond@gmail.com>
2022-07-09 14:06:30 -0700

    Completed Reverse guessing game

--- Week 1/src/challengeQuestions/ReverseGuessingGame.java
+++ Week 1/src/challengeQuestions/ReverseGuessingGame.java
@@ -1,13 +1,25 @@
 package challengeQuestions;
-
 import java.util.Random;
 import java.util.Scanner;
-
 public class ReverseGuessingGame {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Random ran = new Random();
-        int r = ran.nextInt(10)+1;//this is a random variable
-
+        int a = 0;
+        while(true) {
+            Random ran = new Random();
+            int r = ran.nextInt(10) + 1;
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Is it " + r + "? y/n");
+            String guess = sc.nextLine();
+            if(guess.equals("y")){
+                System.out.println("I got your number in " + (a+1) + " guess(es)");
+                break;
+            }
+            else if(guess.equals("n")){
+                a += 1;
+            }
+            else{
+                System.out.println("Invalid answer.");
+            }
+        }
     }
 }
