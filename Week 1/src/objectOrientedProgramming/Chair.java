d57c8a0d22d9dd9033c451f1d3a3af062edfdcbe
victorxheng <victorchengthesecond@gmail.com>
2022-07-13 08:01:29 -0700

    Chair examples

--- Week 1/src/objectOrientedProgramming/Chair.java
+++ Week 1/src/objectOrientedProgramming/Chair.java
@@ -1,4 +1,56 @@
 package objectOrientedProgramming;
 
-public class Chair {
+public class Chair
+{
+    //Credit: Matthew
+    private int numberOfLegs;
+    private boolean isOccupied;
+    private double cost;
+
+    public int getNumberOfLegs() {
+        return numberOfLegs;
+    }
+
+    public boolean isOccupied() {
+        return isOccupied;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setNumberOfLegs(int numberOfLegs) {
+        this.numberOfLegs = numberOfLegs;
+    }
+
+    public void setOccupied(boolean occupied) {
+        isOccupied = occupied;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public Chair(int legs, boolean occupation, double price){
+        isOccupied = occupation;
+        numberOfLegs = legs;
+        cost = price;
+    }
+    public boolean canIBuy(double money){
+        if(money>=cost){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public void destroyChair(){
+        cost = 0;
+        numberOfLegs = 0;
+        isOccupied = false;
+    }
+    public void removeOccupant(){
+        numberOfLegs = 2;
+        isOccupied = false;
+    }
 }
--- Week 1/src/objectOrientedProgramming/ChairMain.java
+++ Week 1/src/objectOrientedProgramming/ChairMain.java
@@ -1,4 +1,13 @@
 package objectOrientedProgramming;
 
-public class ChairMain {
+public class ChairMain
+{
+    //credit: Matthew
+    public static void main(String[] args)
+    {
+        Chair janschair = new Chair(4,true,100);
+        Chair aaronsChair = new Chair(4,true,20);
+        aaronsChair.destroyChair();
+        janschair.removeOccupant();
+    }
 }
