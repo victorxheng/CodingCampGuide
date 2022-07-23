f88392e8091dd8f0e1538e23c2237bd823c4d81b
victorxheng <victorchengthesecond@gmail.com>
2022-07-22 21:26:24 -0700

    solutions

--- Week 1/src/treesGraphsRecursion/recursion/RecursionChallenges.java
+++ Week 1/src/treesGraphsRecursion/recursion/RecursionChallenges.java
@@ -4,10 +4,39 @@ public class RecursionChallenges {
     public static void main(String[] args) {
         //call the method
         System.out.println(factorial(5));
+
+        int[] a = {5, 6, 2, 4, 3, 9, 1};
+        printElementInArray(a, 0);
+        System.out.println(sumOfElements(a, 0));
+        System.out.println(sum(10));
+        System.out.println(sumOfGaps(a, 1));//odd
+        System.out.println(sumOfGaps(a, 0));//even
     }
     //create the method
     public static int factorial(int n){
         if(n <= 1) return 1;
         return n * factorial(n-1);
+    }
+    public static void printElementInArray(int[] a, int index){
+        if(index >= a.length){
+            return;
+        }
+        System.out.println(a[index]);
+        printElementInArray(a, index + 1);
+    }
+
+    public static int sumOfElements(int[] a, int index){
+        if(index >= a.length){
+            return 0;
+        }
+        return a[index] + sumOfElements(a, index + 1);
+    }
+    public static int sum(int n){
+        if(n <= 1) return 1;
+        return n + sum(n - 1);
+    }
+    public static int sumOfGaps(int[] a, int index){
+        if(index >= a.length) return 0;
+        return a[index] + sumOfGaps(a, index + 2);
     }
 }
--- Week 1/src/treesGraphsRecursion/trees/MyTreeMain.java
+++ Week 1/src/treesGraphsRecursion/trees/MyTreeMain.java
@@ -5,13 +5,19 @@ public class MyTreeMain {
         MyTreeNode root = new MyTreeNode(5);
         root.left = new MyTreeNode(3);
         root.right = new MyTreeNode(6);
-        root.right.right.left = new MyTreeNode(5);
         root.left.left = new MyTreeNode(7);
         root.left.right = new MyTreeNode(120);
         root.right.left = new MyTreeNode(3);
         root.right.right.right = new MyTreeNode(5);
+        root.right.right.left = new MyTreeNode(5);
         root.right.right = new MyTreeNode(2);
         root.right.left.right = new MyTreeNode(5);
+    }
+
+    public static void printLeafNodes(MyTreeNode node){
+        if(node.left == null && node.right == null) System.out.println(node.data);
+        if(node.left != null) printLeafNodes(node.left);
+        if(node.right != null) printLeafNodes(node.right);
     }
     public static int sumTree(MyTreeNode node){
         if(node == null) return 0;
