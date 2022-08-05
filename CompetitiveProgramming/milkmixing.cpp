4493b10fdc7d4cd9b2edd04a8280cf024cb0a820
victorxheng <victorchengthesecond@gmail.com>
2022-08-04 22:41:16 -0700

    milk mixing improved solution

--- CompetitiveProgramming/milkmixing.cpp
+++ CompetitiveProgramming/milkmixing.cpp
@@ -1,27 +1,46 @@
-#include <fstream>
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-int main() {
-  ofstream fout("mixmilk.out");
-  ifstream fin("mixmilk.in");
-  int aye, bee, ci, sizea, sizeb, sizec;
-  fin >> sizea >> aye >> sizeb >> bee >> sizec >> ci;
-  for (int i = 0; i < 100; i++) {
-    if (bee + aye <= sizeb) {
-      bee += aye;
-      aye = 0;
-    } else {
-      aye -= (sizeb - bee);
-      bee = sizeb;
+int main()
+{    ifstream fin ("mixmilk.in");
+    ofstream fout ("mixmilk.out");
+    int firstBucketSize, firstBucketAmount, secondBucketSize, secondBucketAmount, thirdBucketSize, thirdBucketAmount;
+    fin >> firstBucketSize >> firstBucketAmount >> secondBucketSize >> secondBucketAmount >> thirdBucketSize >> thirdBucketAmount;
+    int currentBucket = 1;
+    for(int i = 0; i < 100; i++){
+        if(currentBucket % 3 == 1){
+            if((secondBucketAmount + firstBucketAmount) <= secondBucketSize){
+                secondBucketAmount += firstBucketAmount;
+                firstBucketAmount = 0;
+            }
+            else{
+                firstBucketAmount -= secondBucketSize - secondBucketAmount;
+                secondBucketAmount = secondBucketSize;
+            }
+        }
+        if(currentBucket % 3 == 2){
+            if((thirdBucketAmount + secondBucketAmount) <= thirdBucketSize){
+                thirdBucketAmount += secondBucketAmount;
+                secondBucketAmount = 0;
+            }
+            else{
+                secondBucketAmount -= thirdBucketSize - thirdBucketAmount;
+                thirdBucketAmount = thirdBucketSize;
+            }
+        }
+        if(currentBucket % 3 == 0){
+            if((firstBucketAmount + thirdBucketAmount) <= firstBucketSize){
+                firstBucketAmount += thirdBucketAmount;
+                thirdBucketAmount = 0;
+            }
+            else{
+                thirdBucketAmount -= firstBucketSize - firstBucketAmount;
+                firstBucketAmount = firstBucketSize;
+            }
+        }
+        currentBucket++;
     }
-    swap(aye, bee);
-    swap(ci, bee);
-    swap(sizea, sizeb);
-    swap(sizeb, sizec);
-  }
-  fout << ci << "\n";
-  fout << aye << "\n";
-  fout << bee << "\n";
-  return 0;
+    fout << firstBucketAmount << endl << secondBucketAmount << endl << thirdBucketAmount;
+    return 0;
 }
