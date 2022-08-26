ca57413d0622a927ee0ee16fb5b4d61b52ecbc53
victorxheng <victorchengthesecond@gmail.com>
2022-08-25 22:36:29 -0700

    Timer App react

--- Week 8; Web Development/web-app/src/App.js
+++ Week 8; Web Development/web-app/src/App.js
@@ -1,24 +1,102 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from 'react';
+import { useState, createRef } from 'react';
+
+let totalTime;
+let interval;
 
 function App() {
-  const [hour, setHour] = useState(0);
-  const [minute, setMinute] = useState(0);
-  const [second, setSecond] = useState(0);
+  // creates hour, and the setter for hour
+  const [hour, setHour] = useState('');
+  const [minute, setMinute] = useState('');
+  const [second, setSecond] = useState('');
+  const [elapsed, setElapsed] = useState(0);
+  const [running, setRunning] = useState(false);
+  const [paused, setPaused] = useState(false);
+  const button = createRef();
+  const changeHour = (evt) => {
+    setHour(evt.target.value.replace(/\D/g, ''));
+  }
+  const changeMinute = (evt) => {
+    setMinute(evt.target.value.replace(/\D/g, ''));
+  }
+  const changeSecond = (evt) => {
+    setSecond(evt.target.value.replace(/\D/g, ''));
+  }
+  const tick = () => {
+    setElapsed(x => {
+      if (x === totalTime) {
+        clearInterval(interval);
+        return x;
+      } else {
+        return x + 1;
+      }
+    })
+  }
+  const start = () => {
+    if (running) {
+      if (paused) {
+        // go
+        button.current.classList.replace("start", "stop");
+        button.current.innerText = "Stop";
+        interval = setInterval(tick, 1000);
+        setPaused(false);
+      } else {
+        // stop
+        button.current.classList.replace("stop", "start");
+        button.current.innerText = "Start";
+        clearInterval(interval);
+        setPaused(true);
+      }
+    } else {
+      button.current.classList.replace("start", "stop");
+      button.current.innerText = "Stop";
+      setRunning(true);
+      setPaused(false);
+      let h = hour.length > 0 ? parseInt(hour, 10) : 0;
+      let m = minute.length > 0 ? parseInt(minute, 10) : 0;
+      let s = second.length > 0 ? parseInt(second, 10) : 0;
+
+      totalTime = h * 3600 + m * 60 + s;
+      setElapsed(0);
+      interval = setInterval(tick, 1000);
+    }
+  }
+  const reset = () => {
+    button.current.classList.replace("stop", "start");
+    button.current.innerText = "Start";
+    clearInterval(interval);
+    setPaused(false);
+    setRunning(false);
+  }
+  const getTimeString = () => {
+    let timeLeft = totalTime - elapsed;
+    let h = (timeLeft / 3600 | 0)
+      .toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
+    let m = (timeLeft / 60 % 60 | 0)
+      .toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
+    let s = (timeLeft % 60 | 0)
+      .toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
+    return `${h} : ${m} : ${s}`
+  }
   return (
-    <div className="App">
-      <h1>Timer</h1>
-        <h1>hour&nbsp;&nbsp;&nbsp;:&nbsp;&nbsp;&nbsp;minute&nbsp;&nbsp;&nbsp;:&nbsp;&nbsp;&nbsp;second</h1>
-        <div className="countdown-timer-container">
-            <input type="number" id="hourinput"  onkeypress = "return isNumberKey(event)"/>
-            <input type="number" id="minuteinput"  onkeypress = "return isNumberKey(event)"/>
-            <input type="number" id="secondinput"  onkeypress = "return isNumberKey(event)"/>
+    <div className="OuterDiv">
+      <h1>Timer App</h1>
+      {running ?
+        <h1 className="timer">{getTimeString()}</h1>
+        :
+        <div className="container">
+          <input type="text" className="input" placeholder='h' value={hour} onChange={changeHour} />
+          :
+          <input type="text" className="input" placeholder='m' value={minute} onChange={changeMinute} />
+          :
+          <input type="text" className="input" placeholder='s' value={second} onChange={changeSecond} />
         </div>
-        <div class = "countdown-timer-container">
-            <button onclick = "onstart()">Start</button>
-            <button onclick = "stop()">Stop</button>
-        </div>
+      }
+      <div className="container">
+        <button className="gradient__btn start" onClick={start} ref={button}>Start</button>
+        <button className="gradient__btn reset" onClick={reset}>Reset</button>
+      </div>
     </div>
   );
 }
